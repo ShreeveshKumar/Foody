d9f34550d3407beca64cbc466664b7ae48c325f4
wait <shreeveshkr@gmail.com>
2023-07-12 17:16:51 +0530

    completed

--- foody/src/Home.jsx
+++ foody/src/Home.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./Home.css";
 // import "./share.png";
 import Navbar from "./Navbar";
-
+// import { Link } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 // const img = require("./share.png");
 
 
-const share = () => {
-  return <>alert("Share this app with your friends");</>;
-};
+// const share = () => {
+//   return <>alert("Share this app with your friends");</>;
+// };
 
 const Body = () => {
   return (
@@ -22,18 +23,21 @@ const Body = () => {
     >
       <h1>Reflect on your cravings </h1>
 
-
       <section className="question">
         <h1>What role does </h1>
         <h1>food play in your life ?</h1>
       </section>
 
-        <section className="answer">
-            <h3>Take the first step toward understanding your relationship with food <br /> and get suggestions and recipes to help nourish your mind and body </h3>
-            </section>
-
-        <button className="btn">Get Started</button>
-
+      <section className="answer">
+        <h3>
+          Take the first step toward understanding your relationship with food{" "}
+          <br /> and get suggestions and recipes to help nourish your mind and
+          body{" "}
+        </h3>
+      </section>
+      {/* <Link to="/q1"> */}
+      <button className="btn">Get Started</button>
+      {/* </Link> */}
     </div>
   );
 };
@@ -43,6 +47,7 @@ function Home() {
     <div>
       <Navbar />
       <Body />
+      
     </div>
   );
 }
